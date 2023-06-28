ce5899109edfddf24fa63201ce9d1904843f9c70
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-06-28 18:38:06 +0800

    chore: update 图标

--- src/data.ts
+++ src/data.ts
@@ -1,3 +1,5 @@
+import { apple } from './apple'
+
 const Waving_Hand = ['👋', '👋🏼', '👋🏽', '👋🏾', '👋🏿']
 const Raised_Back_Of_Hand = ['🤚🏼', '🤚🏻', '🤚🏽', '🤚🏾', '🤚🏿']
 const Hand_with_Fingers_Splayed = ['🖐️', '🖐🏼', '🖐🏽', '🖐🏾', '🖐🏿']
@@ -7,17 +9,204 @@ const Rightwards_Hand = ['🫱', '🫱🏼', '🫱🏽', '🫱🏾', '🫱🏿']
 const Leftwards_Hand = ['🫲', '🫲🏼', '🫲🏽', '🫲🏾', '🫲🏿']
 const Palm_Down_Hand = ['🫳', '🫳🏼', '🫳🏽', '🫳🏾', '🫳🏿']
 const Palm_Up_Hand = ['🫴', '🫴🏼', '🫴🏼', '🫴🏾', '🫴🏿']
+const OK_Hand = ['👌', '👌🏼', '👌🏽', '👌🏾', '👌🏿']
+const Pinched_Fingers = ['🤌', '🤌🏼', '🤌🏽', '🤌🏾', '🤌🏿']
+const Pinching_Hand = ['🤏', '🤌🏼', '🤏🏼', '🤏🏾', '🤏🏿']
+const Victory_Hand = ['✌️', '✌🏼', '✌🏽', '✌🏾', '✌🏿']
+const Crossed_Fingers = ['🤞', '🤞🏼', '🤞🏽', '🤞🏾', '🤞🏿']
+const Hand_with_Index_Finger_and_Thumb_Crossed = ['🫰', '🫰🏼', '🫰🏼', '🫰🏾', '🫰🏿']
+const Love_You_Gesture = ['🤟', '🤟🏼', '🤟🏽', '🤟🏾', '🤟🏿']
+const Sign_of_the_Horns = ['🤘', '🤘🏼', '🤘🏽', '🤘🏾', '🤘🏿']
+const Call_Me_Hand = ['🤙', '🤙🏼', '🤙🏽', '🤙🏾', '🤙🏿']
+const Backhand_Index_Pointing_Left = ['👈', '👈🏼', '👈🏽', '👈🏾', '👈🏿']
+const Backhand_Index_Pointing_Right = ['👉', '👉🏼', '👉🏽', '👉🏾', '👉🏿']
+const Backhand_Index_Pointing_Up = ['👆', '👆🏼', '👆🏼', '👆🏾', '👆🏿']
+const Middle_Finger = ['🖕', '🖕🏼', '🖕🏽', '🖕🏾', '🖕🏿']
+const Backhand_Index_Pointing_Down = ['👇', '👇🏼', '👇🏽', '👇🏾', '👇🏿']
+const Index_Pointing_Up = ['☝️', '☝🏼', '☝🏽', '☝🏾', '☝🏿']
+const Index_Pointing_at_the_Viewer = ['🫵', '🫵🏼', '🫵🏽', '🫵🏾', '🫵🏿']
+const Thumbs_Up = ['👍', '👍🏼', '👍🏽', '👍🏾', '👍🏾']
+const Thumbs_Down = ['👎', '👎🏼', '👎🏽', '👎🏾', '👎🏿']
+const Raised_Fist = ['✊', '✊🏼', '✊🏽', '✊🏾', '✊🏿']
+const Oncoming_Fist = ['👊', '👊🏼', '👊🏽', '👊🏾', '👊🏿']
+const Left_Facing_Fist = ['🤛', '🤛🏼', '🤛🏽', '🤛🏾', '🤛🏿']
+const Right_Facing_Fist = ['🤜', '🤜🏼', '🤜🏽', '🤜🏾', '🤜🏿']
+const Clapping_Hands = ['👏', '👏🏼', '👏🏽', '👏🏾', '👏🏿']
+const Raising_Hands = ['🙌', '🙌🏼', '🙌🏽', '🙌🏾', '🙌🏿']
+const Heart_Hands = ['🫶', '🫶🏼', '🫶🏽', '🫶🏾', '🫶🏿']
+const Open_Hands = ['👐', '👐🏼', '👐🏽', '👐🏾', '👐🏿']
+const Palms_Up_Together = ['🤲', '🤲🏼', '🤲🏽', '🤲🏾', '🤲🏿']
+const Handshake = ['🤝', '🤝🏼', '🤝🏽', '🤝🏾', '🤝🏿']
+const Folded_Hands = ['🙏', '🙏🏼', '🙏🏽', '🙏🏾', '🙏🏿']
+const Writing_Hand = ['✍🏻', '✍🏼', '✍🏽', '✍🏾', '✍🏿']
+const Nail_Polish = ['💅', '💅🏼', '💅🏽', '💅🏾', '💅🏿']
+const Selfie = ['🤳', '🤳', '🤳🏽', '🤳🏾', '🤳🏿']
+const Flexed_Biceps = ['💪', '💪🏼', '💪🏼', '💪🏽', '💪🏿']
+const Leg = ['🦵', '🦵🏼', '🦵🏽', '🦵🏾', '🦵🏿']
+const Foot = ['🦶', '🦶🏼', '🦶🏽', '🦶🏾', '🦶🏿']
+const Ear = ['👂', '👂🏼', '👂🏽', '👂🏾', '👂🏿']
+const Ear_with_Hearing_Aid = ['🦻', '🦻🏼', '🦻🏽', '🦻🏾', '🦻🏿']
+const Nose = ['👃', '👃', '👃🏽', '👃🏾', '👃🏿']
+const Baby = ['👶', '👶🏼', '👶🏽', '👶🏾', '👶🏿']
+const Child = ['🧒', '🧒🏼', '🧒🏽', '🧒🏾', '🧒🏿']
+const Boy = ['👦', '👦🏼', '👦🏽', '👦🏾', '👦🏿']
+const Girl = ['👧', '👧🏼', '👧🏽', '👧🏾', '👧🏿']
+const Person = ['🧑', '🧑🏼', '🧑🏽', '🧑🏽', '🧑🏿']
+const Man = ['👨', '👨🏼', '👨🏽', '👨🏾', '👨🏿']
+const Woman = ['👩', '👩🏼', '👩🏼', '👩🏾', '👩🏿']
+const Older_Person = ['🧓', '🧓🏼', '🧓🏽', '🧓🏾', '🧓🏿']
+const Old_Man = ['👴', '👴🏼', '👴🏽', '👴🏽', '👴🏿']
+const Old_Woman = ['👵', '👵🏼', '👵🏽', '👵🏾', '👵🏿']
+const Person_Frowning = ['🙍', '🙍🏻', '🙍🏼', '🙍🏾', '🙍🏿']
+const Man_Frowning = ['🙍‍♂️', '🙍🏼‍♂️', '🙍🏽‍♂️', '🙍🏾‍♂️', '🙍🏿‍♂️']
+const Woman_Frowning = ['🙍‍♀️', '🙍🏼‍♀️', '🙍🏽‍♀️', '🙍🏾‍♀️', '🙍🏿‍♀️']
+const Person_Pouting = ['🙍', '🙍🏼', '🙍🏽', '🙍🏾', '🙍🏿']
+const Man_Pouting = ['🙎‍♂️', '🙎🏼‍♂️', '🙎🏽‍♂️', '🙎🏾‍♂️', '🙎🏾‍♂️']
+const Woman_Pouting = ['🙎‍♀️', '🙎🏼‍♀️', '🙎🏽‍♀️', '🙎🏽‍♀️', '🙎🏿‍♀️']
+const Person_Gesturing_No = ['🙅', '🙅🏼', '🙅🏽', '🙅🏾', '🙅🏿']
+const Man_Gesturing_No = ['🙅‍♂️', '🙅‍♂️', '🙅🏽‍♂️', '🙅🏾‍♂️', '🙅🏿‍♂️']
+const Woman_Gesturing_No = ['🙅‍♀️', '🙅🏼‍♀️', '🙅🏽‍♀️', '🙅🏽‍♀️', '🙅🏿‍♀️']
+const Person_Gesturing_OK = ['🙆', '🙆🏼', '🙆🏽', '🙆🏾', '🙆🏾']
+const Woman_Gesturing_OK = ['🙆‍♀️', '🙆🏼‍♀️', '🙆🏽‍♀️', '🙆🏾‍♀️', '🙆🏿‍♀️']
+const Man_Gesturing_OK = ['🙆‍♂️', '🙆🏼‍♂️', '🙆🏽‍♂️', '🙆🏾‍♂️', '🙆🏿‍♂️']
+const Person_Tipping_Hand = ['💁', '💁🏼', '💁🏽', '💁🏾', '💁🏿']
+const Man_Tipping_Hand = ['💁‍♂️', '💁🏼‍♂️', '💁🏽‍♂️', '💁🏾‍♂️', '💁🏿‍♂️']
+const Woman_Tipping_Hand = ['💁‍♀️', '💁🏼‍♀️', '💁🏽‍♀️', '💁🏾‍♀️', '💁🏿‍♀️']
+const Person_Raising_Hand = ['🙋', '🙋🏼', '🙋🏽', '🙋🏾', '🙋🏿']
+const Man_Raising_Hand = ['🙋‍♂️', '🙋🏼‍♂️', '🙋🏽‍♂️', '🙋🏾‍♂️', '🙋🏿‍♂️']
+const Woman_Raising_Hand = ['🙋‍♀️', '🙋🏼‍♀️', '🙋🏽‍♀️', '🙋🏾‍♀️', '🙋🏿‍♀️']
+const Deaf_Person = ['🧏', '🧏🏼', '🧏🏽', '🧏🏾', '🧏🏾']
+const Deaf_Man = ['🧏‍♂️', '🧏🏼‍♂️', '🧏🏽‍♂️', '🧏🏾‍♂️', '🧏🏿‍♂️']
+const Deaf_Woman = ['🧏‍♀️', '🧏🏼‍♀️', '🧏🏽‍♀️', '🧏🏾‍♀️', '🧏🏿‍♀️']
+const Person_Bowing = ['🙇', '🙇🏼', '🙇🏽', '🙇🏾', '🙇🏿']
+const Man_Bowing = ['🙇‍♂️', '🙇🏼‍♂️', '🙇🏽‍♂️', '🙇🏾‍♂️', '🙇🏿‍♂️']
+const Woman_Bowing = ['🙇🏻‍♀️', '🙇🏼‍♀️', '🙇🏽‍♀️', '🙇🏾‍♀️', '🙇🏿‍♀️']
+const Person_Facepalming = ['🤦', '🤦🏼', '🤦🏽', '🤦🏾', '🤦🏿']
+const Man_Facepalming = ['🤦‍♂️', '🤦🏼‍♂️', '🤦🏽‍♂️', '🤦🏾‍♂️', '🤦🏿‍♂️']
+const Woman_Facepalming = ['🤦‍♀️', '🤦🏼‍♀️', '🤦🏽‍♀️', '🤦🏾‍♀️', '🤦🏿‍♀️']
+const Person_Shrugging = ['🤷', '🤷🏼', '🤷🏽', '🤷🏾', '🤷🏿']
+const Man_Shrugging = ['🤷‍♂️', '🤷‍♂️', '🤷🏽‍♂️', '🤷🏾‍♂️', '🤷🏿‍♂️']
+const Woman_Shrugging = ['🤷‍♀️', '🤷🏼‍♀️', '🤷🏽‍♀️', '🤷🏾‍♀️', '🤷🏿‍♀️']
+const Man_Health_Worker = ['👨‍⚕️', '👨🏼‍⚕️', '👨🏽‍⚕️', '👨🏾‍⚕️', '👨🏿‍⚕️']
+const Woman_Health_Worker = ['👩‍⚕️', '👩🏼‍⚕️', '👩🏼‍⚕️', '👩🏾‍⚕️', '👩🏿‍⚕️']
+const Student = ['🧑‍🎓', '🧑🏼‍🎓', '🧑🏽‍🎓', '🧑🏾‍🎓', '🧑🏿‍🎓']
+const Man_Student = ['👨‍🎓', '👨🏼‍🎓', '👨🏽‍🎓', '👨🏾‍🎓', '👨🏿‍🎓']
+const Woman_Student = ['👩‍🎓', '👩🏼‍🎓', '👩🏽‍🎓', '👩🏾‍🎓', '👩🏿‍🎓']
+const Teacher = ['🧑‍🏫', '🧑🏼‍🏫', '🧑🏽‍🏫', '🧑🏾‍🏫', '🧑🏿‍🏫']
+const Man_Teacher = ['👨‍🏫', '👨🏼‍🏫', '👨🏽‍🏫', '👨🏾‍🏫', '👨🏿‍🏫']
+const Woman_Teacher = ['👩‍🏫', '👩🏼‍🏫', '👩🏼‍🏫', '👩🏾‍🏫', '👩🏿‍🏫']
+const Judge = ['🧑‍⚖️', '🧑🏼‍⚖️', '🧑🏽‍⚖️', '🧑🏾‍⚖️', '🧑🏿‍⚖️']
+const Man_Judge = ['👨‍⚖️', '👨‍⚖️', '👨🏽‍⚖️', '👨🏾‍⚖️', '👨🏿‍⚖️']
+const Woman_Judge = ['👩‍⚖️', '👩🏼‍⚖️', '👩🏽‍⚖️', '👩🏾‍⚖️', '👩🏿‍⚖️']
+const Farmer = ['🧑‍🌾', '🧑🏼‍🌾', '🧑🏼‍🌾', '🧑🏽‍🌾', '🧑🏿‍🌾']
+const Man_Farmer = ['👨‍🌾', '👨🏼‍🌾', '👨🏽‍🌾', '👨🏾‍🌾', '👨🏿‍🌾']
+const Woman_Farmer = ['👨🏿‍🌾', '👩🏼‍🌾', '👩🏽‍🌾', '👩🏾‍🌾', '👩🏾‍🌾']
+// Cook
 
 export const data = [
-  ...transfer(':Waving Hand', Waving_Hand),
-  ...transfer(':Raised Back of Hand', Raised_Back_Of_Hand),
-  ...transfer(':Hand with Fingers Splayed', Hand_with_Fingers_Splayed),
-  ...transfer(':Raised Hand', Raised_Hand),
-  ...transfer(':Vulcan Salute', Vulcan_Salute),
-  ...transfer(':Rightwards Hand', Rightwards_Hand),
-  ...transfer(':Leftwards Hand', Leftwards_Hand),
-  ...transfer(':Palm Down Hand', Palm_Down_Hand),
-  ...transfer(':Palm Up Hand', Palm_Up_Hand),
+  ...transfer('Waving Hand', Waving_Hand),
+  ...transfer('Raised Back of Hand', Raised_Back_Of_Hand),
+  ...transfer('Hand with Fingers Splayed', Hand_with_Fingers_Splayed),
+  ...transfer('Raised Hand', Raised_Hand),
+  ...transfer('Vulcan Salute', Vulcan_Salute),
+  ...transfer('Rightwards Hand', Rightwards_Hand),
+  ...transfer('Leftwards Hand', Leftwards_Hand),
+  ...transfer('Palm Down Hand', Palm_Down_Hand),
+  ...transfer('Palm Up Hand', Palm_Up_Hand),
+  ...transfer('OK Hand', OK_Hand),
+  ...transfer('Pinched Fingers', Pinched_Fingers),
+  ...transfer('Pinching Hand', Pinching_Hand),
+  ...transfer('Victory Hand', Victory_Hand),
+  ...transfer('Crossed Fingers', Crossed_Fingers),
+  ...transfer('Hand with Index Finger and Thumb Crossed', Hand_with_Index_Finger_and_Thumb_Crossed),
+  ...transfer('Love You Gesture', Love_You_Gesture),
+  ...transfer('Sign of the Horns', Sign_of_the_Horns),
+  ...transfer('Call Me Hand', Call_Me_Hand),
+  ...transfer('Backhand Index Pointing Left', Backhand_Index_Pointing_Left),
+  ...transfer('Backhand Index Pointing Right', Backhand_Index_Pointing_Right),
+  ...transfer('Backhand Index Pointing Up', Backhand_Index_Pointing_Up),
+  ...transfer('Middle Finger', Middle_Finger),
+  ...transfer('Backhand Index Pointing Down', Backhand_Index_Pointing_Down),
+  ...transfer('Index Pointing Up', Index_Pointing_Up),
+  ...transfer('Index Pointing at the Viewer', Index_Pointing_at_the_Viewer),
+  ...transfer('Thumbs Up', Thumbs_Up),
+  ...transfer('Thumbs Down', Thumbs_Down),
+  ...transfer('Raised Fist', Raised_Fist),
+  ...transfer('Oncoming Fist', Oncoming_Fist),
+  ...transfer('Left Facing Fist', Left_Facing_Fist),
+  ...transfer('Right Facing Fist', Right_Facing_Fist),
+  ...transfer('Clapping Hands', Clapping_Hands),
+  ...transfer('Raising Hands', Raising_Hands),
+  ...transfer('Heart Hands', Heart_Hands),
+  ...transfer('Open Hands', Open_Hands),
+  ...transfer('Palms Up Together', Palms_Up_Together),
+  ...transfer('Handshake', Handshake),
+  ...transfer('Folded Hands', Folded_Hands),
+  ...transfer('Writing Hand', Writing_Hand),
+  ...transfer('Nail Polish', Nail_Polish),
+  ...transfer('Selfie', Selfie),
+  ...transfer('Flexed Biceps', Flexed_Biceps),
+  ...transfer('Leg', Leg),
+  ...transfer('Foot', Foot),
+  ...transfer('Ear', Ear),
+  ...transfer('Ear with Hearing Aid', Ear_with_Hearing_Aid),
+  ...transfer('Nose', Nose),
+  ...transfer('Baby', Baby),
+  ...transfer('Child', Child),
+  ...transfer('Boy', Boy),
+  ...transfer('Girl', Girl),
+  ...transfer('Person', Person),
+  ...transfer('Man', Man),
+  ...transfer('Woman', Woman),
+  ...transfer('Older Person', Older_Person),
+  ...transfer('Old Man', Old_Man),
+  ...transfer('Old Woman', Old_Woman),
+  ...transfer('Person Frowning', Person_Frowning),
+  ...transfer('Man Frowning', Man_Frowning),
+  ...transfer('Woman Frowning', Woman_Frowning),
+  ...transfer('Person Pouting', Person_Pouting),
+  ...transfer('Man Pouting', Man_Pouting),
+  ...transfer('Woman Pouting', Woman_Pouting),
+  ...transfer('Person Gesturing No', Person_Gesturing_No),
+  ...transfer('Man Gesturing No', Man_Gesturing_No),
+  ...transfer('Woman Gesturing No', Woman_Gesturing_No),
+  ...transfer('Person Gesturing OK', Person_Gesturing_OK),
+  ...transfer('Woman Gesturing OK', Woman_Gesturing_OK),
+  ...transfer('Man Gesturing OK', Man_Gesturing_OK),
+  ...transfer('Person Tipping Hand', Person_Tipping_Hand),
+  ...transfer('Man Tipping Hand', Man_Tipping_Hand),
+  ...transfer('Woman Tipping Hand', Woman_Tipping_Hand),
+  ...transfer('Person Raising Hand', Person_Raising_Hand),
+  ...transfer('Man Raising Hand', Man_Raising_Hand),
+  ...transfer('Woman_Raising Hand', Woman_Raising_Hand),
+  ...transfer('Deaf Person', Deaf_Person),
+  ...transfer('Deaf Man', Deaf_Man),
+  ...transfer('Deaf Woman', Deaf_Woman),
+  ...transfer('Person Bowing', Person_Bowing),
+  ...transfer('Man Bowing', Man_Bowing),
+  ...transfer('Woman Bowing', Woman_Bowing),
+  ...transfer('Person Facepalming', Person_Facepalming),
+  ...transfer('Man Facepalming', Man_Facepalming),
+  ...transfer('Woman Facepalming', Woman_Facepalming),
+  ...transfer('Person Shrugging', Person_Shrugging),
+  ...transfer('Man Shrugging', Man_Shrugging),
+  ...transfer('Woman Shrugging', Woman_Shrugging),
+  ...transfer('Man Health Worker', Man_Health_Worker),
+  ...transfer('Woman Health Worker', Woman_Health_Worker),
+  ...transfer('Person Facepalming', Person_Facepalming),
+  ...transfer('Student', Student),
+  ...transfer('Man Student', Man_Student),
+  ...transfer('Woman Student', Woman_Student),
+  ...transfer('Teacher', Teacher),
+  ...transfer('Man Teacher', Man_Teacher),
+  ...transfer('Woman Teacher', Woman_Teacher),
+  ...transfer('Judge', Judge),
+  ...transfer('Man Judge', Man_Judge),
+  ...transfer('Woman Judge', Woman_Judge),
+  ...transfer('Farmer', Farmer),
+  ...transfer('Man Farmer', Man_Farmer),
+  ...transfer('Woman Farmer', Woman_Farmer),
+  ...apple,
 ]
 
 function transfer(base: string, data: any) {
