bea63bda57086b5655e1e9a8a93325d7bb22fda0
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-07-06 14:49:42 +0800

    chore: 使用:去触发选择表情

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 import type * as vscode from 'vscode'
-import { createCompletionItem, registerCompletionItemProvider } from '@vscode-use/utils'
+import { createCompletionItem, createRange, getSelection, registerCommand, registerCompletionItemProvider, updateText } from '@vscode-use/utils'
 import { data } from './data'
 
 export function activate(context: vscode.ExtensionContext) {
-  const cacheMap = data.map(([content, detail]) => createCompletionItem({ content: `${content}${detail}`, detail, snippet: detail }))
-  context.subscriptions.push(registerCompletionItemProvider(['markdown', 'plaintext'], () => cacheMap, [' ']))
+  context.subscriptions.push(registerCommand('emoji.select', (emoji) => {
+    const { line, lineText } = getSelection()!
+    updateText((edit) => {
+      edit.replace(createRange([line, 0], [line, lineText.length]), lineText.replace(`:${emoji}`, emoji))
+    })
+  }))
+
+  const cacheMap = data.map(([content, detail]) => {
+    const item = createCompletionItem({ content: `${content}${detail}`, detail, snippet: detail, type: 4 })
+    item.command = {
+      title: detail,
+      command: 'emoji.select',
+      arguments: [detail],
+    }
+    return item
+  })
+  context.subscriptions.push(registerCompletionItemProvider(['markdown', 'plaintext'], () => cacheMap, [':']))
 }
 
 export function deactivate() {
